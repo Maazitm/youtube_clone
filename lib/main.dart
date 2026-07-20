a834608cd707f7037104a14b9e8682b9e77aa2d1
Maazitm <siddifurqan@gmail.com>
2026-07-20 10:25:34 +0530

    1) login screen added 2) splas screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
+import 'package:youtube/firebase_options.dart';
 import 'package:youtube/screens/shortsScreen.dart';
-import 'package:youtube/screens/testScreen.dart';
+import 'package:youtube/screens/splashScreen.dart';
 import 'package:youtube/screens/videoPlayerScreen.dart';
 
-void main() {
+void main() async {
+  //to ensure the flutter framework is fully initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //  Start Firebase using the automatic platform options
+  //  Check if Firebase is already initialized
+  if (Firebase.apps.isEmpty) {
+    // 2. If empty, initialize it
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print("Firebase just initialized!");
+  } else {
+    // 3. If not empty, skip initialization
+    print("Firebase was already initialized.");
+  }
+
   runApp(const MyApp());
 }
 
@@ -22,7 +39,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => Testscreen(),
+        '/': (context) => SplashScreen(),
         '/videoPlayerScreen': (context) => Videoplayerscreen(),
         '/shortsScreen': (context) => ShortsScreen(),
       },
